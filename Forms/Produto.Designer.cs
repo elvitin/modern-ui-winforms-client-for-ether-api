6743ccb5f8d9edf45cdc7b64805061be73d972c7
taveira <victortavi@outlook.com>
2022-10-04 15:09:38 -0300

    Branch AppEngVersion criada
    Ramificação da aplicação final criada, essa ramifição, unira a API, o padrão MVP, a interface FlatUI, com embasamento no que já foi feito em AppForm

--- Forms/Produto.Designer.cs
+++ Forms/Produto.Designer.cs
@@ -29,151 +29,20 @@
     private void InitializeComponent()
     {
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Produto));
-      this.TlpCadastro = new System.Windows.Forms.TableLayoutPanel();
-      this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
-      this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
-      this.kpmTextBox3 = new FlatUI.CustomControls.KpmTextBox();
-      this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-      this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
-      this.kpmTextBox2 = new FlatUI.CustomControls.KpmTextBox();
-      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-      this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-      this.kpmTextBox1 = new FlatUI.CustomControls.KpmTextBox();
-      this.TlpCadastro.SuspendLayout();
-      this.tableLayoutPanel5.SuspendLayout();
-      this.tableLayoutPanel6.SuspendLayout();
-      this.tableLayoutPanel3.SuspendLayout();
-      this.tableLayoutPanel4.SuspendLayout();
-      this.tableLayoutPanel1.SuspendLayout();
-      this.tableLayoutPanel2.SuspendLayout();
       this.SuspendLayout();
-      // 
-      // TlpCadastro
-      // 
-      resources.ApplyResources(this.TlpCadastro, "TlpCadastro");
-      this.TlpCadastro.Controls.Add(this.tableLayoutPanel5, 0, 2);
-      this.TlpCadastro.Controls.Add(this.tableLayoutPanel3, 0, 0);
-      this.TlpCadastro.Controls.Add(this.tableLayoutPanel1, 0, 1);
-      this.TlpCadastro.Name = "TlpCadastro";
-      // 
-      // tableLayoutPanel5
-      // 
-      resources.ApplyResources(this.tableLayoutPanel5, "tableLayoutPanel5");
-      this.tableLayoutPanel5.Controls.Add(this.tableLayoutPanel6, 1, 0);
-      this.tableLayoutPanel5.Name = "tableLayoutPanel5";
-      // 
-      // tableLayoutPanel6
-      // 
-      resources.ApplyResources(this.tableLayoutPanel6, "tableLayoutPanel6");
-      this.tableLayoutPanel6.Controls.Add(this.kpmTextBox3, 0, 1);
-      this.tableLayoutPanel6.Name = "tableLayoutPanel6";
-      // 
-      // kpmTextBox3
-      // 
-      this.kpmTextBox3.BackColor = System.Drawing.SystemColors.Window;
-      this.kpmTextBox3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
-      this.kpmTextBox3.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
-      this.kpmTextBox3.BorderRadius = 5;
-      this.kpmTextBox3.BorderSize = 1;
-      resources.ApplyResources(this.kpmTextBox3, "kpmTextBox3");
-      this.kpmTextBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(27)))));
-      this.kpmTextBox3.Multiline = false;
-      this.kpmTextBox3.Name = "kpmTextBox3";
-      this.kpmTextBox3.PasswordChar = false;
-      this.kpmTextBox3.PlaceHolderColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(152)))), ((int)(((byte)(165)))));
-      this.kpmTextBox3.PlaceHolderText = "";
-      this.kpmTextBox3.Texts = "";
-      this.kpmTextBox3.UnderlinedStyle = false;
-      // 
-      // tableLayoutPanel3
-      // 
-      resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
-      this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 1, 0);
-      this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-      // 
-      // tableLayoutPanel4
-      // 
-      resources.ApplyResources(this.tableLayoutPanel4, "tableLayoutPanel4");
-      this.tableLayoutPanel4.Controls.Add(this.kpmTextBox2, 0, 1);
-      this.tableLayoutPanel4.Name = "tableLayoutPanel4";
-      // 
-      // kpmTextBox2
-      // 
-      this.kpmTextBox2.BackColor = System.Drawing.SystemColors.Window;
-      this.kpmTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
-      this.kpmTextBox2.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
-      this.kpmTextBox2.BorderRadius = 5;
-      this.kpmTextBox2.BorderSize = 1;
-      resources.ApplyResources(this.kpmTextBox2, "kpmTextBox2");
-      this.kpmTextBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(27)))));
-      this.kpmTextBox2.Multiline = false;
-      this.kpmTextBox2.Name = "kpmTextBox2";
-      this.kpmTextBox2.PasswordChar = false;
-      this.kpmTextBox2.PlaceHolderColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(152)))), ((int)(((byte)(165)))));
-      this.kpmTextBox2.PlaceHolderText = "";
-      this.kpmTextBox2.Texts = "";
-      this.kpmTextBox2.UnderlinedStyle = false;
-      // 
-      // tableLayoutPanel1
-      // 
-      resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-      this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
-      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-      // 
-      // tableLayoutPanel2
-      // 
-      resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
-      this.tableLayoutPanel2.Controls.Add(this.kpmTextBox1, 0, 1);
-      this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-      // 
-      // kpmTextBox1
-      // 
-      this.kpmTextBox1.BackColor = System.Drawing.SystemColors.Window;
-      this.kpmTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
-      this.kpmTextBox1.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
-      this.kpmTextBox1.BorderRadius = 5;
-      this.kpmTextBox1.BorderSize = 1;
-      resources.ApplyResources(this.kpmTextBox1, "kpmTextBox1");
-      this.kpmTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(27)))));
-      this.kpmTextBox1.Multiline = false;
-      this.kpmTextBox1.Name = "kpmTextBox1";
-      this.kpmTextBox1.PasswordChar = false;
-      this.kpmTextBox1.PlaceHolderColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(152)))), ((int)(((byte)(165)))));
-      this.kpmTextBox1.PlaceHolderText = "";
-      this.kpmTextBox1.Texts = "";
-      this.kpmTextBox1.UnderlinedStyle = false;
       // 
       // Produto
       // 
       resources.ApplyResources(this, "$this");
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
       this.BackColor = System.Drawing.Color.White;
-      this.Controls.Add(this.TlpCadastro);
       this.ForeColor = System.Drawing.Color.White;
       this.Name = "Produto";
       this.Load += new System.EventHandler(this.Produto_Load);
-      this.TlpCadastro.ResumeLayout(false);
-      this.tableLayoutPanel5.ResumeLayout(false);
-      this.tableLayoutPanel6.ResumeLayout(false);
-      this.tableLayoutPanel3.ResumeLayout(false);
-      this.tableLayoutPanel4.ResumeLayout(false);
-      this.tableLayoutPanel1.ResumeLayout(false);
-      this.tableLayoutPanel2.ResumeLayout(false);
       this.ResumeLayout(false);
 
     }
 
         #endregion
-
-        private TableLayoutPanel TlpCadastro;
-        private TableLayoutPanel tableLayoutPanel1;
-        private TableLayoutPanel tableLayoutPanel2;
-        private CustomControls.KpmTextBox kpmTextBox1;
-        private TableLayoutPanel tableLayoutPanel5;
-        private TableLayoutPanel tableLayoutPanel6;
-        private CustomControls.KpmTextBox kpmTextBox3;
-        private TableLayoutPanel tableLayoutPanel3;
-        private TableLayoutPanel tableLayoutPanel4;
-        private CustomControls.KpmTextBox kpmTextBox2;
     }
 }
